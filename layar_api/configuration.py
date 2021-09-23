f2b73d473f68ae9070311d6845546866d4d5647b
Build <build@vyasa.com>
2021-09-23 20:09:52 +0000

    Release v1.0.701

--- layar_api/configuration.py
+++ layar_api/configuration.py
@@ -252,7 +252,7 @@ class Configuration(six.with_metaclass(TypeWithDefault, object)):
                "OS: {env}\n"\
                "Python Version: {pyversion}\n"\
                "Version of the API: 2.0\n"\
-               "SDK Package Version: v1.0.700".\
+               "SDK Package Version: v1.0.701".\
                format(env=sys.platform, pyversion=sys.version)
 
     def fetch_access_token(self, client_id, client_secret):
