7ffcad51605297c1074ace5872bcc24a6feb4d1e
Build <build@vyasa.com>
2022-06-14 00:00:02 +0000

    Release v1.0.760

--- layar_api/api_client.py
+++ layar_api/api_client.py
@@ -72,7 +72,7 @@ class ApiClient(object):
             self.default_headers[header_name] = header_value
         self.cookie = cookie
         # Set default User-Agent.
-        self.user_agent = 'Swagger-Codegen/v1.0.759/python'
+        self.user_agent = 'Swagger-Codegen/v1.0.760/python'
 
     def __del__(self):
         self.pool.close()
