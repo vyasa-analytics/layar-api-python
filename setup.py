b85bc1f365e4dbd242e95a4b8d8bee8965116161
Build <build@vyasa.com>
2023-09-28 21:41:04 +0000

    Release v1.0.839

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@
 from setuptools import setup, find_packages  # noqa: H301
 
 NAME = "layar-api"
-VERSION = "v1.0.838"
+VERSION = "v1.0.839"
 # To install the library, run the following
 #
 # python setup.py install
